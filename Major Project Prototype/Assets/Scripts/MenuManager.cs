87e38fefd577772cf26e93192f233cc206c944d2
erfangz <126913041+erfangz@users.noreply.github.com>
2023-04-13 18:45:00 +0200

    menu updates
    added sub menu structure
    add "togglemenu" function to navigate between sub menus

--- Major Project Prototype/Assets/Scripts/MenuManager.cs
+++ Major Project Prototype/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Opens a selected Sub Menu from the Main Menu Button
+    /// </summary>
+    /// <param name="Menu">The menu to be opened on pressing the relevant button</param>
+    public void ToggleMenu(GameObject Menu)
     {
-        
+        Menu.SetActive(true);
     }
 }
