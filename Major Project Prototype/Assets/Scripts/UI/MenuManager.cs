a678b4e6927bc19c2aa93d65267b66fd4ac8ea61
erfangz <126913041+erfangz@users.noreply.github.com>
2023-06-05 16:23:40 +0200

    add main menu functionality
    add menu manager
    add transitions from main menu buttons into menu screens
    add return button to return to main menu

---
+++ Major Project Prototype/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NaughtyAttributes;
+
+public class MenuManager : MonoBehaviour
+{
+    #region Objects
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject returnButton;
+
+    [Header("Main Menu Sub Menus")]
+    [SerializeField]
+    private GameObject newGameScreen;
+    [SerializeField]
+    private GameObject continueGameScreen;
+    [SerializeField]
+    private GameObject optionsScreen;
+    [SerializeField]
+    private GameObject quitGamePopUp;
+
+
+    #endregion
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    #region Button Methods
+    // functions open the respective menu while disabling the main menu
+
+    [Button]
+    public void NewGame()
+    {
+        mainMenu.SetActive(false);
+        newGameScreen.SetActive(true);
+        returnButton.SetActive(true);
+    }
+
+    [Button]
+    public void ContinueGame()
+    {
+        mainMenu.SetActive(false);
+        continueGameScreen.SetActive(true);
+        returnButton.SetActive(true);
+    }
+
+    [Button]
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        optionsScreen.SetActive(true);
+        returnButton.SetActive(true);
+    }
+
+    [Button]
+    public void PromptQuitGame()
+    {
+        quitGamePopUp.SetActive(true);
+    }
+
+    /// <summary>
+    /// Returns to Main Menu
+    /// </summary>
+    [Button]
+    public void Return()
+    {
+        // disable all other menus
+        continueGameScreen.SetActive(false);
+        newGameScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+        quitGamePopUp.SetActive(false);
+
+        // enable main menu
+        mainMenu.SetActive(true);
+
+        // return button deactivated when in main menu
+        returnButton.SetActive(false);
+    }
+    #endregion
+}
