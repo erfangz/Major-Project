10edc8e821dba4cccace56fcdbe7e01670b38684
erfangz <126913041+erfangz@users.noreply.github.com>
2023-06-23 16:27:59 +0200

    proto ai update
    add enemy tag
    add behaviour tree
    add fov check behaviour, implement behaviour
    add patrol behaviour

---
+++ Major Project Prototype/Assets/Scripts/ProtoAI/Patrol.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class Patrol : Action
+{
+    #region Variables
+    public float MoveSpeed;
+    public Transform[] WayPoints;
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
