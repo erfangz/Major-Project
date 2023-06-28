6047ba67ef7db98e1c53ac1f63e1bc4a11e44466
erfangz <126913041+erfangz@users.noreply.github.com>
2023-06-28 14:56:36 +0200

    proto ai update
    add chasing behaviour
    ai now functionally checks for targets in its fov and chases them, behaviours running seperately at correct times and conditions

---
+++ Major Project Prototype/Assets/Scripts/ProtoAI/Chase.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+/// <summary>
+/// Action node to chase a target acquired by FOV_Check Node
+/// </summary>
+public class Chase : Action
+{
+    #region References
+    public SharedTransform Target;
+    public float MoveSpeed;
+    #endregion
+
+    #region Methods
+    public override TaskStatus OnUpdate()
+    {
+        #region Chase Logic
+        // check distance to target
+        if (Vector3.SqrMagnitude(transform.position - Target.Value.position) < 0.1f)
+            return TaskStatus.Success;
+
+        // move to target location
+        transform.position = Vector3.MoveTowards(transform.position, Target.Value.position, MoveSpeed * Time.deltaTime);
+
+        // look at the target
+        transform.LookAt(Target.Value);
+
+        return TaskStatus.Running;
+        #endregion
+    }
+    #endregion
+}
