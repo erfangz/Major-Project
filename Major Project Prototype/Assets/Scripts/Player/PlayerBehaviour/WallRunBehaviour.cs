23add618e0067fc9f315b80ce41594be8dacb977
erfangz <126913041+erfangz@users.noreply.github.com>
2023-07-03 11:35:07 +0200

    Import commissioned rifle asset

--- Major Project Prototype/Assets/Scripts/Player/PlayerBehaviour/WallRunBehaviour.cs
+++ Major Project Prototype/Assets/Scripts/Player/PlayerBehaviour/WallRunBehaviour.cs
@@ -17,7 +17,7 @@ public class WallRunBehaviour : MonoBehaviour
     public MoveState State;
 
     [BoxGroup("Object Layers")]
-    public LayerMask Ground;    
+    public LayerMask Ground;
     [BoxGroup("Object Layers")]
     public LayerMask Wall;
 
@@ -30,30 +30,51 @@ public class WallRunBehaviour : MonoBehaviour
     [BoxGroup("Object Checks")]
     public bool OnRightSideWall;
 
+    public bool WallRunning;
+
+    [BoxGroup("Object Checks")]
+    public float DistanceToWall;
+
+    private RaycastHit wall_L;
+    private RaycastHit wall_R;
+
     [BoxGroup("References")]
     public Transform Orientation;
     [BoxGroup("References")]
     public Rigidbody Rb;
+
+    float verticalInput;
+    float horizontalInput;
     #endregion
 
     #region Methods
-    void Update()
+    void FixedUpdate()
     {
         #region Checks
         // ground check
         Grounded = Physics.CheckSphere(GroundCheck.transform.position, 0.1f, Ground);
 
         // wall check
-        //OnLeftSideWall = Physics.Raycast(transform.position, -Orientation.right, out );
+        OnLeftSideWall = Physics.Raycast(transform.position, -Orientation.right, out wall_L, DistanceToWall, Wall);
+        OnRightSideWall = Physics.Raycast(transform.position, Orientation.right, out wall_R, DistanceToWall, Wall);
         #endregion
+    }
+
+    void Update()
+    {
+
     }
 
     /// <summary>
     /// Controls Player Move State
     /// </summary>
-    void SwitchStates()
+    void SwitchStates(MoveState state)
     {
+        // Wall Running State
+        WallRunning = true;
+        state = MoveState.ONWALL;
 
+        // Normal Movement State
     }
 
     #region Movement
@@ -63,6 +84,11 @@ public class WallRunBehaviour : MonoBehaviour
     void OnWallMove()
     {
 
+
+        if (OnLeftSideWall || OnRightSideWall && verticalInput > 0 && !Grounded)
+        {
+            State = MoveState.ONWALL;
+        }
     }
 
     /// <summary>
